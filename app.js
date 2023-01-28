a2ce130d3a58883d2ecf51245805251490a0a915
Rashid9977 <119620703+Rashid9977@users.noreply.github.com>
2023-01-28 14:43:08 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const loginForm = document.getElementById('login-form')
+const errorMsg = document.getElementById('error-msg');
+const successMsg = document.getElementById('success-msg');
+const btnSubmit = document.getElementById('btn-submit');
+
+
+btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const email = loginForm.email.value;
+    const password = loginForm.pwd.value;
+
+    if (email === 'test@gmail.com' || password === 'test1234') {
+        successMsg.classList.remove('visually-hidden')
+        errorMsg.classList.add('visually-hidden');
+    }
+    else {
+        successMsg.classList.add('visually-hidden');
+        errorMsg.classList.remove('visually-hidden');
+    }
+})
